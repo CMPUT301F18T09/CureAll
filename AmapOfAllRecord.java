b260569bd1d731b3ff581266735a6dd24dc0fc53
Nora Zhang <35251132+lllor@users.noreply.github.com>
2018-11-01 21:12:33 -0600

    Update AmapOfAllRecord.java

--- AmapOfAllRecord.java
+++ AmapOfAllRecord.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
 class AmapOfAllRecord{
-	public static String username
-	ArrayList<String> LocationOfallrecords = new ArrayList<String>()
+	public static String username//the user name which assigned from profile class
+	ArrayList<String> LocationOfallrecords = new ArrayList<String>()//get the List of Location for this particular patient
 	public void getLocationOfallrecords(username){LocationOfallrecords = gson.(....)}
 	
-	public Map(){
+	public Map(){//open the map
 		Intent.Map; 
 		Map.locationset = LocationOfallrecords
-		Map.type = 2
+		Map.type = 2//assigen type=2, map will show a serious of flag, each belongs to a record
 	}
 }
