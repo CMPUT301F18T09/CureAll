5b9aba958932ad58654b985e2834d53c85a4ba01
Xianhang Li <33477009+reversedArrow@users.noreply.github.com>
2018-11-02 01:15:36 -0600

    paper cut doll
    paper cut doll

---
+++ UnitTest/PaperDollPhotoTest.java
@@ -0,0 +1,36 @@
+package com.example.shabi;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+
+public  class PaperDollPhotoTest {
+    @Test
+
+    public void addGeoLocationPhoto(){
+        String photoLocation = "photoSize";
+        double photoSize = "354654651316516546516.1641654654654654";
+        double photoWidth = "123456789101112131415.1114664646465465465465";
+        double photoLenth = "65465423132165465498465.13216546546498498465415132";
+
+        SRT test =new SRT(photoLocation,photoSize,photoWidth,photoLenth);
+
+        assertEquals(SRT.getPhotoLocation(),photoLocation);
+        assertEquals(SRT.getphotoSize(),photoSize);
+        assertEquals(SRT.getphotoWidth(),photoWidth);
+        assertEquals(SRT.getphotoLenth(),photoLenth);
+
+    }
+    @Test
+    public void getAPointLocation(){
+        String aPointLocation = "point";
+        GeoLocation geoLocation = new geoLocation("point");
+        assertEquals( geoLocation.getLocation, aPointLocation);
+    }
+
+
+
+}
