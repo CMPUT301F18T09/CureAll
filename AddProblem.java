e12697b80ef7c86f6278e2e97b506cd391d8f3ce
Nora Zhang <35251132+lllor@users.noreply.github.com>
2018-11-01 21:03:22 -0600

    commit

--- AddProblem.java
+++ AddProblem.java
@@ -1,5 +1,5 @@
 class AddProblem {
-	ArrayList<Problems> problemList = new ArrayList<Problems>()
+	ArrayList<Problems> problemList = new ArrayList<Problems>()//this list use to save the list of problem which read from the file, then append user's enter at the end of list, the store it to file later
 	
 	public ReadFromFIle(problemList = gson.fromJson(....))
 
