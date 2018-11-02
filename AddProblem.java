965e6ff8d552c4aed4bde85c8874949a0caf7c69
Nora Zhang <35251132+lllor@users.noreply.github.com>
2018-11-01 21:04:43 -0600

    Update AddProblem.java

--- AddProblem.java
+++ AddProblem.java
@@ -1,10 +1,10 @@
 class AddProblem {
 	ArrayList<Problems> problemList = new ArrayList<Problems>()//this list use to save the list of problem which read from the file, then append user's enter at the end of list, the store it to file later
 	
-	public ReadFromFIle(problemList = gson.fromJson(....))
+	public ReadFromFIle(problemList = gson.fromJson(....))//read Problems from file/online, covert the context to problem type
 
 	public SaveToFile(){
-		problemList.add(new(Problems(String Title, String descrption)))
+		problemList.add(new(Problems(String Title, String descrption)))//add new problem
 		save(....)
 	}
 	
