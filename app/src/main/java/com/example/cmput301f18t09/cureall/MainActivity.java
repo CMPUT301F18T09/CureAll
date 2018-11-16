ffc29152b7c6ecf52b160706fede7d52e9cdde15
JialinX <jialin@ualberta.ca>
2018-11-15 17:35:24 -0700

    For get current location

--- app/src/main/java/com/example/cmput301f18t09/cureall/MainActivity.java
+++ app/src/main/java/com/example/cmput301f18t09/cureall/MainActivity.java
@@ -1,13 +1,125 @@
 package com.example.cmput301f18t09.cureall;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.GetMapButton);
+        textView = (TextView) findViewById(R.id.View);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                textView.append("\n "+location.getLatitude()+" "+location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.INTERNET}, 10);
+            return;
+        }
+        //
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 10:
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    configureButton();
+                return;
+    }
+}
+
+    private void configureButton() {
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+            }
+        });
+    }
+
+
+//    int PLACE_PICKER_REQUEST = 1;
+//    private TextView GeoLocation;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        GeoLocation = (TextView)findViewById(R.id.GetMapButton);
+//        GeoLocation.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+//                Intent MapActivity;
+//
+//                try {
+//                    MapActivity = builder.build((Activity) getApplicationContext());
+//                    startActivityForResult(MapActivity,PLACE_PICKER_REQUEST);
+//                } catch (GooglePlayServicesRepairableException e) {
+//                    e.printStackTrace();
+//                } catch (GooglePlayServicesNotAvailableException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//        });
+//    }
+//
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+//        if (requestCode == PLACE_PICKER_REQUEST){
+//            if (resultCode == RESULT_OK){
+//                Place place = PlacePicker.getPlace(this,data);
+//                String geolocation = String.format("%s",place.getAddress());//(String) place.getAddress();
+//                GeoLocation.setText(geolocation);
+//            }
+//        }
+//    }
 }
---
+++ app/src/main/java/com/example/cmput301f18t09/cureall/Map.java
@@ -0,0 +1,13 @@
+package com.example.cmput301f18t09.cureall;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Map extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map);
+    }
+}
