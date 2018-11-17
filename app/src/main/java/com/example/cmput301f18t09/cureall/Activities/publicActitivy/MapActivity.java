72b1125f2330a8ebd6d5ac1d1933a3aa6f523815
apple <lhong2@ualberta.ca>
2018-11-17 15:55:38 -0700

    fixed bug

--- app/src/main/java/com/example/cmput301f18t09/cureall/Activities/publicActitivy/MapActivity.java
+++ app/src/main/java/com/example/cmput301f18t09/cureall/Activities/publicActitivy/MapActivity.java
@@ -97,16 +97,6 @@ public class MapActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (ActivityCompat.checkSelfPermission(MapActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MapActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
                 locationManager.requestLocationUpdates("gps", 0, 0, locationListener);
             }
         });
