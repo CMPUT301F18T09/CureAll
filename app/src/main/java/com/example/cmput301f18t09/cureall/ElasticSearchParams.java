171c944c068c0e2d8af7515e2e5cb6955b466b16
JialinX <35158650+JialinX@users.noreply.github.com>
2018-11-19 23:18:37 -0700

    add javadoc for model

--- app/src/main/java/com/example/cmput301f18t09/cureall/ElasticSearchParams.java
+++ app/src/main/java/com/example/cmput301f18t09/cureall/ElasticSearchParams.java
@@ -1,7 +1,21 @@
+/**
+ * Class name: ElasticSearchParams
+ *
+ * Version: v1.0.0
+ *
+ * Date: November 1, 2018
+ *
+ * Copyright (c) 2018. Team09, F18 CMPUT301, All rights reserved.
+ */
 package com.example.cmput301f18t09.cureall;
 
 import java.util.ArrayList;
-
+/**
+ * Model class for recording CareProvider
+ *
+ * @author Ruiqin, Pi
+ * @version 1.0.0
+ */
 public class ElasticSearchParams {
     Patient patient;
     String username;
@@ -13,6 +27,12 @@ public class ElasticSearchParams {
     ArrayList<String> patients;
     String problemid;
 
+    /**
+     * one Init for class ElasticSearchParams
+     * @param username  the username used in elastic search
+     * @param problem   searched problem
+     * @param id        id
+     */
     public ElasticSearchParams(String username,Problem problem,String id){
         this.username = username;
         this.problem = problem;
@@ -20,29 +40,52 @@ public class ElasticSearchParams {
 
     }
 
+    /**
+     * one init for class ElasticSearchParams
+     * @param num
+     * @param problem
+     * @param Patientid
+     */
     public ElasticSearchParams(Integer num,Problem problem,String Patientid){
         this.num = num;
         this.problem = problem;
         this.id = id;
     }
 
+    /**
+     * one init for class ElasticSearchParams
+     * @param username
+     * @param patients
+     */
     public ElasticSearchParams(String username, ArrayList<String> patients){
         this.username = username;
         this.patients = patients;
     }
 
+    /**
+     * one init for class ElasticSearchParams
+     * @param username
+     * @param record
+     * @param problemid
+     */
     public ElasticSearchParams(String username, Record record, String problemid){
         this.username = username;
         this.record = record;
         this.problemid = problemid;
     }
 
+    /**
+     * one init for class ElasticSearchParams
+     * @param username
+     * @param problemid
+     */
     public ElasticSearchParams(String username, String problemid){
         this.username = username;
         this.problemid = problemid;
     }
 
-/*    public ElasticSearchParams(String problemid)
+/*  may use later
+    public ElasticSearchParams(String problemid)
     {
         this.problemid = problemid;
     }*/
